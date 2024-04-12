5ec71f269ba29e7a65403a008dd8625924f9d766
Lil Kev <kjolly544@gmail.com>
2024-04-12 00:44:19 -0400

    Update InvMenuTransactionResult.php

--- src/muqsit/invmenu/transaction/InvMenuTransactionResult.php
+++ src/muqsit/invmenu/transaction/InvMenuTransactionResult.php
@@ -1,40 +1,25 @@
 <?php
 
-/*
- *  ___            __  __
- * |_ _|_ ____   _|  \/  | ___ _ __  _   _
- *  | || '_ \ \ / / |\/| |/ _ \ '_ \| | | |
- *  | || | | \ V /| |  | |  __/ | | | |_| |
- * |___|_| |_|\_/ |_|  |_|\___|_| |_|\__,_|
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * @author Muqsit
- * @link http://github.com/Muqsit
- *
-*/
-
 declare(strict_types=1);
 
 namespace muqsit\invmenu\transaction;
 
 use Closure;
+use pocketmine\player\Player;
 
 final class InvMenuTransactionResult{
 
-	/** @var bool */
-	private $cancelled;
+	/** @var (Closure(Player) : void)|null */
+	public ?Closure $post_transaction_callback = null;
 
-	/** @var Closure|null */
-	private $post_transaction_callback;
+	public function __construct(
+		readonly public bool $cancelled
+	){}
 
-	public function __construct(bool $cancelled){
-		$this->cancelled = $cancelled;
-	}
-
+	/**
+	 * @deprecated Access {@see InvMenuTransactionResult::$cancelled} directly
+	 * @return bool
+	 */
 	public function isCancelled() : bool{
 		return $this->cancelled;
 	}
@@ -45,16 +30,18 @@ final class InvMenuTransactionResult{
 	 * Useful for sending forms and other stuff that cant be sent right
 	 * after closing inventory.
 	 *
-	 * @param Closure|null $callback
+	 * @param (Closure(Player) : void)|null $callback
 	 * @return self
-	 *
-	 * @phpstan-param Closure(\pocketmine\Player) : void $callback
 	 */
 	public function then(?Closure $callback) : self{
 		$this->post_transaction_callback = $callback;
 		return $this;
 	}
 
+	/**
+	 * @deprecated Access {@see InvMenuTransactionResult::$post_transaction_callback} directly
+	 * @return (Closure(Player) : void)|null
+	 */
 	public function getPostTransactionCallback() : ?Closure{
 		return $this->post_transaction_callback;
 	}
